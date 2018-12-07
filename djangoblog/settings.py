30440ca0905d6f6e23fa2e88d92b128b0b159ccd
Peter Nabil <peter@github.com>
2018-12-07 16:21:07 +0200

    the final commit the task is complete

--- djangoblog/settings.py
+++ djangoblog/settings.py
@@ -129,3 +129,9 @@ MEDIA_URL = '/media/'
 LOGIN_REDIRECT_URL = 'blog-home'
 LOGIN_URL = 'login'
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+
+EMAIL_HOST='localhost'
+EMAIL_PORT=1025
+EMAIL_USE_TLS = False
